acc70d62d5b1fd4567677bca23bbb4b526b059aa
raicunicole <raicu_nicole@yahoo.com>
2022-11-02 16:21:16 +0200

    Prima pagina modificata

---
+++ Controllers/ControllerNou.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Raicu_Nicole_Lab1.Controllers
+{
+    public class ControllerNou : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
